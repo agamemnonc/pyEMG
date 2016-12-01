fb2508601abba83994c29ed2fe2e516c2e79d69c
Agamemnon <agamemnon.krasoulis@gmail.com>
2016-12-01 17:05:25 +0000

    small fix in documentation

--- smarthand.py
+++ smarthand.py
@@ -36,7 +36,7 @@ class SmartHand(object):
         self.finger_status_ = np.chararray((n_df,), itemsize=6)
         self.finger_status_[:] = 'steady'
         self.finger_pos_ = np.zeros(n_df, dtype=float) # Finger positions
-        self.finger_set_ = np.zeros(n_df, dtype=float) # Desired finger positions
+        self.finger_set_ = np.zeros(n_df, dtype=float) # Set finger positions
 
         self.pose_ = None
         self.__executing = False
