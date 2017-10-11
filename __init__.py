62411faf8b91d5c74319c080d9be7fb868a44787
Agamemnon <agamemnon.krasoulis@gmail.com>
2017-10-11 13:57:02 +0100

    update init file to include var feature

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
+
 from bin_parm import BinParm
 from datasets import DatasetRaw
 from wiener_filter import WienerFilter
 from smarthand import SmartHand
 from cyberglove import CyberGlove
 from time_buffer import Buffer
-from features_online import get_wamp_feat, get_wl_feat, get_mv_feat, get_int_mode_feat, get_mav_feat, get_logvar_feat, get_ssc_feat, get_ar_feat, get_quantile_feat
+from features_online import get_wamp_feat, get_wl_feat, get_mv_feat, get_int_mode_feat, get_mav_feat, get_var_feat, get_logvar_feat, get_ssc_feat, get_ar_feat, get_quantile_feat
 from emg_filter import emg_filter_bandpass, emg_filter_comb
 from imu_filter import imu_filter_lowpass, imu_filter_highpass, imu_filter_bandpass, imu_filter_comb
 from glove_filter import glove_filter_lowpass
-from utils import interpolate_time_vector, get_number_imu_signals, get_acc_indices, get_gyro_indices, get_mag_indices, get_imu_indices, strip_inactive, get_num_windows, ismember, write_to_txt, stimulus_presentation, dump_raw_data
+from pyEMG.utils import interpolate_time_vector, get_number_imu_signals, get_acc_indices, get_gyro_indices, get_mag_indices, get_imu_indices, strip_inactive, get_num_windows, ismember, write_to_txt, stimulus_presentation, dump_raw_data, RobustMinMaxScaler
 from metrics import vaf_score, vaf_mv_score, balanced_accuracy_score, balanced_log_loss
 from glove_calibration import calibrate_glove
 from robolimb import RoboLimb
@@ -19,11 +20,11 @@ from smoothing import MovingAverage, ExponentialSmoothing, DoubleExponentialSmoo
 
 __all__ = ["BinParm",   "DatasetRaw", "DatasetRaw",
            "WienerFilter", "SmartHand", "RoboLimb", "Buffer", "emg_filter_comb", 
-           "emg_filter_bandpass", "get_wamp_feat", "get_wl_feat", "get_logvar_feat", 
+           "emg_filter_bandpass", "get_wamp_feat", "get_wl_feat", "get_var_feat", "get_logvar_feat", 
            "get_wl_feat", "get_int_mode_feat", "get_mv_feat", "get_mav_feat",
            "get_ssc_feat", "get_ar_feat", "get_quantile_feat", "glove_filter_lowpass", "interpolate_time_vector",
            "imu_filter_lowpass", "imu_filter_highpass", "imu_filter_bandpass",
            "imu_filter_comb", "vaf_score", "vaf_mv_score", "balanced_accuracy_score", "balanced_log_loss", 
            "get_number_imu_signals", "get_acc_indices", "get_gyro_indices", "get_mag_indices", "get_imu_indices","calibrate_glove", "strip_inactive", "get_num_windows",
            "MovementCrossValidation", "ismember", "RocThreshold", "control_action", "stimulus_presentation", "write_to_txt", "dump_raw_data",
-		   "DelsysStation", "CyberGlove", "MovingAverage", "ExponentialSmoothing", "DoubleExponentialSmoothing"]
+		   "DelsysStation", "CyberGlove", "MovingAverage", "ExponentialSmoothing", "DoubleExponentialSmoothing", "RobustMinMaxScaler"]
