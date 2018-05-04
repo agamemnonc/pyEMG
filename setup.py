8729664a5138ca8fecb49a2eb42ae13dc0394804
Agamemnon Krasoulis <agamemnonc@yahoo.com>
2018-05-04 10:27:59 +0100

    make python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+""" Setup script for pyEMG package. """
+
+from setuptools import setup
+
+setup(
+    name = "pyEMG",
+    version = "0.0.1",
+    author = "Agamemnon Krasoulis",
+    description = ("EMG processing for myoelectric control in Python"),
+    url = "https://github.com/agamemnonc/pyEMG",
+    packages=['pyEMG']
+)
