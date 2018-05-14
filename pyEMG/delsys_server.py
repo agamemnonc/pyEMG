d77704befd271d1ee93726521b03b7f606dc9cf7
Agamemnon <agamemnon.krasoulis@gmail.com>
2018-05-14 15:11:17 +0100

    python 3 compatibility

--- pyEMG/delsys_server.py
+++ pyEMG/delsys_server.py
@@ -1,4 +1,4 @@
-""" 
+"""
 Delsys Trigno server implementation
 
 Author:
@@ -8,55 +8,57 @@ Modified by:
 Agamemnon Krasoulis
 agamemnon.krasoulis@gmail.com
 
-TODO: 
+TODO:
 Implement DelsysStation for EMG/Acc sensors for Trigno
 
 
 """
 
+from __future__ import print_function, division
 import socket
-import thread
+import threading
 from struct import unpack
 import numpy as np
 import timeit
 from pyEMG.time_buffer import Buffer
+#from pyEMG.stoppable_thread import StoppableThread
 
 
 class DelsysStation(object):
     '''
     Class to receive data from delsys station. Connect to station, buffer received data.
-    
+
     Parameters
     ----------
-    
+
     buffered : boolean
         option for buffering/appending new data
-    
+
     host_ip : string
         IP address of the machine running the Delsys Trigno Server
-    
+
     bufsize : float
         buffer length in seconds
-        
+
     samplesPerPacket : int
         number of samples in each packet received from the Trigno base
-    
+
     Attributes
     ----------
-    
+
     data : numpy array / buffer
         list of buffered data (1st: EMG, 2nd: IMU)
-    
+
     time : numpy array / buffer
-        list of timestamps (1st: EMG, 2nd: IMU)  
+        list of timestamps (1st: EMG, 2nd: IMU)
     '''
     def __init__(self, buffered=True, host_ip = '127.0.0.1', bufsize = 1.,
                  samplesPerPacket = 1, imu_type=None):
-        
+
         self.host = host_ip
         self.dataPort = 50043
         self.imuPort = 50044
-        
+
         self.sdkPort = 50040
         self.emg = None
         self.imu = None
@@ -71,7 +73,7 @@ class DelsysStation(object):
         self.__bytesPerSample = 4
         self.__signalsPerEmgSensor = 1
         if self.imuType == 'raw':
-            self.__signalsPerImuSensor = 9 
+            self.__signalsPerImuSensor = 9
             self.__signalsPerImuSensorTransmitted = 9
         elif self.imuType == 'quat':
             self.__signalsPerImuSensor = 4
@@ -81,7 +83,7 @@ class DelsysStation(object):
             self.__signalsPerImuSensorTransmitted = 5
         else:
             raise ValueError('Unrecognised type of IMU transmission.')
-        
+
         if self.buffered:
             self._emgBufSize = int(np.ceil(self.__emgRate * self.bufsize))
             self._imuBufSize = int(np.ceil(self.__imuRate * self.bufsize))
@@ -94,28 +96,30 @@ class DelsysStation(object):
             np.zeros((self.__numSensors * self.__signalsPerImuSensor,))]
             self.time = [np.zeros((1,)), np.zeros((1,))]
         self.exitFlag = True
-        
+
     def start(self):
         ''' establish connection:
         sdk - port
         emg - port
         imu - port
         '''
-        
+
         self.flush() # Reset buffer
-        print "connect to " + str(self.host)
+        print("connect to " + str(self.host))
         self.sdk = socket.create_connection((self.host, self.sdkPort))
         self.imu = socket.create_connection((self.host, self.imuPort))
         self.emg = socket.create_connection((self.host, self.dataPort))
-        
-        print "connected"
-        self.sdk.send('START\r\n\r\n')
+
+        print("connected")
+        self.sdk.send(b'START\r\n\r\n')
         self.sdk.recv(1024)
         self._startTime = timeit.default_timer()
         self.exitFlag = False
-        thread.start_new_thread(self.networking, (self.emg, 'emg'))
-        thread.start_new_thread(self.networking, (self.imu, 'imu'))
-        
+        self.emg_thread = StoppableThread(target=self.networking, args=(self.emg, 'emg'))
+        self.imu_thread = StoppableThread(target=self.networking, args=(self.imu, 'imu'))
+        self.emg_thread.start()
+        self.imu_thread.start()
+
     def networking(self, server, mode):
         ''' receive packets of data and fill buffer '''
         if mode == 'emg':
@@ -134,9 +138,9 @@ class DelsysStation(object):
             elif self.imuType == 'quat':
                 dummy_cols = self.__signalsPerImuSensorTransmitted * np.arange(self.__numSensors) + 4 # 5th byte is reserved
             elif self.imuType == 'pry':
-                dummy_cols = np.sort(np.concatenate((self.__signalsPerImuSensorTransmitted*np.arange(self.__numSensors)+3, 
+                dummy_cols = np.sort(np.concatenate((self.__signalsPerImuSensorTransmitted*np.arange(self.__numSensors)+3,
                                                      self.__signalsPerImuSensorTransmitted*np.arange(self.__numSensors)+4))) # 4th and 5th bytes are reserved
-            
+
         while not self.exitFlag:
             data = server.recv(recSize)
 
@@ -145,28 +149,32 @@ class DelsysStation(object):
                 data += server.recv(recSize-length)
                 length = len(data)
 
-            data = np.asarray(unpack('<'+'f'*(recSize/self.__bytesPerSample), data))
+            data = np.asarray(unpack('<'+'f'*(int(recSize/self.__bytesPerSample)), data))
             data = data.reshape((shp))
             data = np.delete(data, dummy_cols, axis=1)
             timestamp = np.asarray([timeit.default_timer()])
-            
+
             if self.buffered:
                 self.data[buf_index].push(data)
                 self.time[buf_index].push(np.ones((data.shape[0],1))*timestamp)
             else:
                 self.data[buf_index] = data
                 self.time[buf_index] = timestamp
-            
-    
+
+
     def stop(self):
         ''' close connections to server '''
         self.exitFlag = True
-        self.sdk.send("QUIT\r\n\r\n")
+        self.emg_thread.stop()
+        self.imu_thread.stop()
         self._stopTime = timeit.default_timer()
+        self.sdk.send(b'QUIT\r\n\r\n')
+        time.sleep(0.1) # Wait until the threads are stopped before closing the sockets
         self.emg.close()
         self.sdk.close()
         self.imu.close()
         
+
     def flush(self):
         ''' reset buffer '''
         if self.buffered:
@@ -175,5 +183,5 @@ class DelsysStation(object):
             self.time = [Buffer((self._emgBufSize, 1)), \
             Buffer((self._imuBufSize, 1))]
         else:
-             self.data = [np.zeros((self.__numSensors,)), np.zeros((self.__numSensors*self.__signalsPerImuSensor,))]   
+             self.data = [np.zeros((self.__numSensors,)), np.zeros((self.__numSensors*self.__signalsPerImuSensor,))]
              self.time = [np.zeros((1,)), np.zeros((1,))]
